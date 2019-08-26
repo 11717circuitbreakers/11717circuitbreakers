08de41c72b8887c85e46dd955faf60ea054b2ed3
techygamerkid <54126919+techygamerkid@users.noreply.github.com>
2019-08-26 17:55:41 -0400

    fixed

--- signup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SET /P inputname= Please enter your full name: 
-SET /P email= Please enter an email we can contact you at: 
-SET /P interest= Please tell us why you are interested in robotics: 
-SET /P parent= Could your parents be involved? How?: 
-echo %inputname% at %email%, says "%interest%" about their interest in robotics, and "%parent%" about parent involvement. > "%userprofile%/desktop/robotics-school-demo/signup/%inputname%.txt"
